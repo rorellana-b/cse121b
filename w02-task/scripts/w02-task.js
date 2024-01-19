c4ed0d54b702e4d3637e8e9c2b25522d450c1754
rorellana-b <ronalorellanarbo@gmail.com>
2024-01-18 21:29:37 -0600

    Finished w02-task

---
+++ w02-task/scripts/w02-task.js
@@ -0,0 +1,42 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = 'Ronal Orellana';
+let currentYear = new Date().getFullYear();
+let profilePicture = 'images/picture.png';
+
+
+
+/* Step 3 - Element Variables */
+
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('img');
+
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = `<strong>${currentYear}<strong>`;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', 'Profile image of profilePicture')
+
+
+/* Step 5 - Array */
+
+let food = ['Pizza','Hot Dog','Spaguetti'];
+
+foodElement.innerHTML = food;
+let anotherFood = "chicken";
+food.push(anotherFood);
+foodElement.innerHTML+= `<br>${food}`;
+
+
+
+
+
+
+
+
